c6845dabe32ed10a6f09165ed6a7ac0c242fab25
qpseone2 <qpsaone2@naver.com>
2021-08-04 19:33:35 +0900

    Page 구성 & 검색 기능 구현

---
+++ src/main/java/com/godcoder/myhome/model/Board.java
@@ -0,0 +1,27 @@
+package com.godcoder.myhome.model;
+
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자이상 30자 이하입니다.")
+    private String title;
+    private String content;
+
+}
